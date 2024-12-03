ef80a9f56fa72a09b62f4ac154fe7c612dc1d30c
LABiB-1 <najibmahmudlabib.1@gmail.com>
2024-12-03 09:45:31 +0600

    lab 1 done

---
+++ lab 1/print_num.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a, b;
+    printf("Enter a number:");
+    scanf("%d", &a);
+    printf("Enter a number:");
+    scanf("%d", &b);
+    printf("%d %d\n",a ,b);
+    return 0;
+}
